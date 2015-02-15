45f61f5244cda52e0701be4643fd4c8b015dcb3d
rokkincat <josh@rokkincat.com>
2015-02-15 15:33:56 -0600

    0.1.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule LazyFloat.Mixfile do
 
   def project do
     [app: :ecto_lazy_float,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.0",
      name: "Ecto.LazyFloat",
      description: description,
