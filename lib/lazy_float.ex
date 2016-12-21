3ac7b2338d8b88c5d99fbaf918d2d0ee06ba0863
DanielZendejas <daniel8647@hotmail.com>
2016-12-21 15:39:55 -0600

    Add missing load case for integers

--- lib/lazy_float.ex
+++ lib/lazy_float.ex
@@ -10,20 +10,20 @@ defmodule Ecto.LazyFloat do
       :error   -> :error
     end
   end
-
   def cast(integer) when is_integer(integer) do
     case Float.parse(to_string(integer)) do
       {float, _} -> {:ok, float}
       :error   -> :error
     end
   end
-
   def cast(float) when is_float(float), do: {:ok, float}
-
   def cast(_), do: :error
+
   def blank?(_), do: false
   
   def load(float) when is_float(float), do: {:ok, float}
+  def load(integer) when is_integer(integer), do {:ok, integer / 1}
+  def load(_), do: :error
   
   def dump(float) when is_float(float), do: {:ok, float}
   def dump(_), do: :error
